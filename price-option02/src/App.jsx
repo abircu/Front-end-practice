177247117cbbbf4ab275f082a9694bc9ac26500b
abircu <mdabircse4873@gmail.com>
2024-02-22 22:15:05 +0600

    first commit

---
+++ price-option02/src/App.jsx
@@ -0,0 +1,13 @@
+import "./App.css";
+import DaisyNav from "./Components/DaisyNav";
+
+function App() {
+  return (
+    <>
+      <DaisyNav></DaisyNav>
+      <h1 className="text-3xl">Vite + React</h1>
+    </>
+  );
+}
+
+export default App;
